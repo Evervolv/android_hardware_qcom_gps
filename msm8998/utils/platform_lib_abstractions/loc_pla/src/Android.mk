e872612127cfbf55ceba01e9860e0acc47d64447
Vishwath Mohan <vishwath@google.com>
2017-06-17 10:36:16 -0700

    Remove hardcoded LOCAL_MODULE_PATHS from vendor components. (gps)
    This CL replaces all LOCAL_MODULE_PATH_* definitions for vendor shared
    libraries with LOCAL_VENDOR_MODULE := true instead. This appropriately
    generates sanitized versions of these libraries to /data/asan/* while
    generating the stock versions in /vendor/lib* as desired.
    
    Needed for ASAN builds to work correctly.
    
    Bug: 37740897
    Test: m -j40 && SANITIZE_TARGET="address" m -j40 # shared libs in
    $ANDROID_PRODUCT_OUT/vendor/lib* do not contain asan symbols, while
    the ones in $ANDROID_PRODUCT_OUT/data/asan/vendor/lib* do.
    
    Change-Id: If6a495dd153c47f929de047fd48039b990348720

--- msm8998/utils/platform_lib_abstractions/loc_pla/src/Android.mk
+++ msm8998/utils/platform_lib_abstractions/loc_pla/src/Android.mk
@@ -42,8 +42,7 @@ LOCAL_COPY_HEADERS:= \
         ../include/platform_lib_time.h
 
 LOCAL_MODULE := libloc_pla
-LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_OWNER := qti
 
