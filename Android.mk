730d5a338b83c7b9427b1bebffd8d6712182f71b
Chris Fries <cfries@google.com>
2017-06-30 01:40:10 -0500

    Android.mk: rework to fix sdm845
    Bug: 62287998
    Change-Id: Ie9188a510490ef447a24baca816767ec8d52c915

--- Android.mk
+++ Android.mk
@@ -1,33 +1,28 @@
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
   ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-  LOCAL_PATH := $(call my-dir)
-
+    LOCAL_PATH := $(call my-dir)
     ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 
-      ifneq ($(filter msm8960 apq8064 ,$(TARGET_BOARD_PLATFORM)),)
-        #For msm8960/apq8064 targets
+      ifneq ($(filter apq8064,$(TARGET_BOARD_PLATFORM)),)
+        #For apq8064 use msm8960
         include $(call all-named-subdir-makefiles,msm8960)
-      else ifneq ($(filter msm8084 ,$(TARGET_BOARD_PLATFORM)),)
-        #For msm8084 target
-        include $(call all-named-subdir-makefiles,msm8084)
-      else ifneq ($(filter msm8992 msm8994 ,$(TARGET_BOARD_PLATFORM)),)
-        #For msm8992/msm8994 targets
+      else ifneq ($(filter msm8992,$(TARGET_BOARD_PLATFORM)),)
+        #For msm8992 use msm8994
         include $(call all-named-subdir-makefiles,msm8994)
-      else ifneq ($(filter msm8996 ,$(TARGET_BOARD_PLATFORM)),)
-        #For msm8996 target
-        include $(call all-named-subdir-makefiles,msm8996)
-      else ifneq ($(filter msm8998 ,$(TARGET_BOARD_PLATFORM)),)
-        #For msm8998 target
-        include $(call all-named-subdir-makefiles,msm8998)
+      else ifneq ($(filter msm8960 msm8084 msm8994 msm8996 msm8998 sdm845,$(TARGET_BOARD_PLATFORM)),)
+        #For these, use their platform name as the subdirectory
+        include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
       else ifeq ($(filter msm8916,$(TARGET_BOARD_PLATFORM)),)
-        #For all other targets
+        #For all other targets besides msm8916
         GPS_DIRS=core utils loc_api platform_lib_abstractions etc
         include $(call all-named-subdir-makefiles,$(GPS_DIRS))
       endif #TARGET_BOARD_PLATFORM
 
     else
-      ifneq ($(filter msm8909 msm8226 ,$(TARGET_BOARD_PLATFORM)),)
+      ifneq ($(filter sdm845,$(TARGET_BOARD_PLATFORM)),)
+        include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
+      else ifneq ($(filter msm8909 msm8226 ,$(TARGET_BOARD_PLATFORM)),)
         #For msm8909 target
         GPS_DIRS=msm8909/core msm8909/utils msm8909/loc_api msm8909/etc
         include $(call all-named-subdir-makefiles,$(GPS_DIRS))
@@ -36,10 +31,6 @@ ifneq ($(BOARD_IS_AUTOMOTIVE),true)
         include $(call all-named-subdir-makefiles,$(GPS_DIRS))
       endif
     endif #BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET
-
-    ifneq ($(filter sdm845 ,$(TARGET_BOARD_PLATFORM)),)
-       include $(call all-named-subdir-makefiles,sdm845)
-    endif
 
   endif #BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
 endif
