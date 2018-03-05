875b53bd781ce11b304558b31cb6055f0b939c74
sudarsanarao makupalli <smakup@codeaurora.org>
2018-03-05 08:16:01 +0100

    msm8084: Avoiding race conditions during the pallellized builds in Android system
    Removed relative paths and set Absolute paths in Android.mk file
    and moved corresponding files to right location
    
    Change-Id: I42d1c2d532f9461f2e6cd5f027dfe755e7497f77

--- msm8084/utils/Android.mk
+++ msm8084/utils/Android.mk
@@ -19,7 +19,7 @@ LOCAL_SRC_FILES += \
     linked_list.c \
     loc_target.cpp \
     loc_timer.c \
-    ../platform_lib_abstractions/elapsed_millis_since_boot.cpp \
+    platform_lib_abstractions/elapsed_millis_since_boot.cpp \
     loc_misc_utils.cpp
 
 LOCAL_CFLAGS += \
@@ -35,7 +35,7 @@ LOCAL_LDFLAGS += -Wl,--export-dynamic
 
 ## Includes
 LOCAL_C_INCLUDES:= \
-    $(LOCAL_PATH)/../platform_lib_abstractions
+    $(LOCAL_PATH)/platform_lib_abstractions
 
 LOCAL_COPY_HEADERS_TO:= gps.utils/
 LOCAL_COPY_HEADERS:= \
@@ -46,9 +46,9 @@ LOCAL_COPY_HEADERS:= \
    msg_q.h \
    loc_target.h \
    loc_timer.h \
-   ../platform_lib_abstractions/platform_lib_includes.h \
-   ../platform_lib_abstractions/platform_lib_time.h \
-   ../platform_lib_abstractions/platform_lib_macros.h \
+   platform_lib_abstractions/platform_lib_includes.h \
+   platform_lib_abstractions/platform_lib_time.h \
+   platform_lib_abstractions/platform_lib_macros.h \
    loc_misc_utils.h
 
 LOCAL_MODULE := libgps.utils
