d1648a5720a3f81e1b9176cb279ad89a281f5e32
Arne Coucheron <arco68@gmail.com>
2019-11-28 02:22:57 +0100

    gps: msm8960: Fix compiler warnings
    Change-Id: I4e2033db162af0586ee04a5db26b2137f0f720a4

--- msm8960/loc_api/libloc_api_50001/Android.mk
+++ msm8960/loc_api/libloc_api_50001/Android.mk
@@ -42,8 +42,8 @@ LOCAL_SRC_FILES += \
 LOCAL_CFLAGS += \
      -fno-short-enums \
      -D_ANDROID_ \
-     -Wno-unused-parameter \
      -Wno-error \
+     -Wno-unused-parameter
 
 LOCAL_C_INCLUDES:= \
     $(TARGET_OUT_HEADERS)/gps.utils \
@@ -93,6 +93,7 @@ LOCAL_CFLAGS += \
     -fno-short-enums \
     -D_ANDROID_ \
     -Wno-error \
+    -Wno-unused-parameter
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
 LOCAL_CFLAGS += -DTARGET_USES_QCOM_BSP
